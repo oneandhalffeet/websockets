2be87f5732c5bf78ab3725d9094ce3c510cb3176
unknown <uddeshteke007@gmail.com>
2022-11-28 01:10:20 +0530

    added comments

--- app/app.js
+++ app/app.js
@@ -1,12 +1,23 @@
 const socket = io('ws://localhost:8080');
+// ws stands for websockets
 
 socket.on('message', (text) => {
+  //on getting message from socket
+
   const el = document.createElement('li');
   el.innerHTML = text;
+  // create new list tag element with given message
+
   document.querySelector('ul').appendChild(el);
+  // apend it to the list
 });
 
 document.querySelector('button').onclick = () => {
+  // on clicking submit button
+
   const text = document.querySelector('input').value;
+  // get value of message
+
   socket.emit('message', text);
+  //emit this to socket, so socket will broadcast it
 };
